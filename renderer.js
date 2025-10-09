698747a1fdbf4ba35dbc0ed87996e612ff893062
Alexander Choi <168526775+alicksc@users.noreply.github.com>
2025-10-09 03:18:09 -0700

    Integrate chess logic from chess.js

--- renderer.js
+++ renderer.js
@@ -1,15 +1,22 @@
+import { Chess } from './libs/chess.js';
+
 document.addEventListener("DOMContentLoaded", () => {
   const board = Chessboard('board', {
     position: 'start',
     draggable: true,
     dropOffBoard: 'snapback',  // pieces return if dropped off board
-    pieceTheme: 'libs/pieces/default/{piece}.svg'
+    pieceTheme: 'libs/pieces/default/{piece}.svg',
+    onDrop: (source, target) => {
+        const move = game.move({ from: source, to: target});
+
+        if(move === null) return 'snapback';   // checks for illegal moves
+
+        // Updates UI to match game logic
+        board.position(game.fen(), true);
+    }
   });
 
   // Optional: Connect to chess.js for rules
   const game = new Chess();
 
-  // You can now make moves, e.g.:
-  // game.move("e4");
-  // board.position(game.fen());
 });
