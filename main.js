0f9913a46b27e0a863cdab9cfca11834e825a5a1
Alexander Choi <168526775+alicksc@users.noreply.github.com>
2025-10-09 01:24:30 -0700

    Setup: Initialize Electron app structure and link files

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    resizable: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true, // allows you to use Node in the browser (for now)
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  app.quit();
+});
